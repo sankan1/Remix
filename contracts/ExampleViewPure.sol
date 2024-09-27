5c91762dd128935bd13dee369a0af3e0e249ab39
sankan1 <kangur.sander@gmail.com>
2024-09-27 08:28:33 +0300

    29 Writing and reading

---
+++ contracts/ExampleViewPure.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.14;
+
+contract ExampleViewPure {
+    uint public myStorageVariable;
+
+    function getMyStorageVariable() public view returns(uint) {
+        return myStorageVariable;
+    }
+
+    function getAddition(uint a, uint b) public pure returns(uint) {
+        return a + b;
+    }
+
+    function setMyStorageVariable(uint _new) public {
+        myStorageVariable = _new;
+    }
+}
